2e19a7d72daecd53fdf566a1741e6ce6b8ac75b6
assertivoalgoritmo-ctrl <assertivoalgoritmo@gmail.com>
2026-02-22 02:47:27 +0000

    Fix Codec import: use barrel import to resolve dart:convert conflict

--- amavel_app/lib/presentation/pages/main_chat_page.dart
+++ amavel_app/lib/presentation/pages/main_chat_page.dart
@@ -4,7 +4,7 @@ import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_sound/public/flutter_sound_recorder.dart';
+import 'package:flutter_sound/flutter_sound.dart';
 import 'package:just_audio/just_audio.dart' as ja;
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
